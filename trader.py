e927aae654d0b365f75fa4630928e03007bd915b
Alex Klein <hello@gnarlex.com>
2023-07-19 19:02:31 +0200

    use new alpaca API, add ci config

--- trader.py
+++ trader.py
@@ -1,6 +1,13 @@
-import alpaca_trade_api as tradeapi
-import numpy as np
 import time
+from os import getenv
+from datetime import datetime, timedelta
+import numpy as np
+
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.timeframe import TimeFrame
 
 '''
     This is a trading script that you can run in the cloud and customize with your own trading algorithms.
@@ -11,39 +18,41 @@ import time
     Alternatively, edit the `get_data`, `buy`, and `sell` functions to connect your own APIs.
 '''
 
-SEC_KEY = ''
-PUB_KEY = ''
-BASE_URL = 'https://paper-api.alpaca.markets'
-api = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
+PUB_KEY = getenv('ALPACA_KEY')
+SEC_KEY = getenv('ALPACA_SECRET')
 
-def get_data():
-    # Returns a an numpy array of the closing prices of the past 5 minutes
-    market_data = api.get_barset(symb, 'minute', limit=5)
+trade = TradingClient(PUB_KEY, SEC_KEY)
+data = StockHistoricalDataClient(PUB_KEY, SEC_KEY)
 
-    close_list = []
-    for bar in market_data[symb]:
-        close_list.append(bar.c)
-
-    close_list = np.array(close_list, dtype=np.float64)
-
+def get_data(symb):
+    # Returns a numpy array of the closing prices of the past 15 minutes
+    market_data = data.get_stock_bars(StockBarsRequest(
+        symbol_or_symbols = symb,
+        timeframe = TimeFrame.Minute,
+        start = datetime.now() - timedelta(minutes=30),
+        end = datetime.now() - timedelta(minutes=15),
+    ))
+    
+    close_list = market_data.df['close']
     return close_list
 
-def buy(q, s): # Returns nothing, makes call to buy stock
-    api.submit_order(
+# Returns nothing, makes call to buy stock
+def buy(q, s):
+    trade.submit_order(order_data=MarketOrderRequest(
         symbol=s,
         qty=q,
         side='buy',
-        type='market',
         time_in_force='gtc'
-    )
-def sell(q, s): # Returns nothing, makes call to sell stock
-    api.submit_order(
+    ))
+
+# Returns nothing, makes call to sell stock
+def sell(q, s):
+    trade.submit_order(order_data=MarketOrderRequest(
         symbol=s,
         qty=q,
         side='sell',
-        type='market',
         time_in_force='gtc'
-    )
+    ))
 
 symb = "SPY" # Ticker of stock you want to trade
 pos_held = False
@@ -52,10 +61,10 @@ while True:
     print("")
     print("Checking Price")
 
-    close_list = get_data()
+    close_list = get_data(symb)
 
     ma = np.mean(close_list)
-    last_price = close_list[4]
+    last_price = close_list[-1]
 
     print("Moving Average: " + str(ma))
     print("Last Price: " + str(last_price))
